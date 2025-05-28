fbc8bc7598a9a8e414560ec07b1fb649ffab81ba
tanaysali <tanaysali2002@gmail.com>
2025-05-29 05:07:42 +0530

    Updates

--- components/experienceSection/ExperienceTopMiddle.jsx
+++
@@ -1,9 +0,0 @@
-const ExperienceTopMiddle = () => {
-  return (
-    <div className="lg:w-[35%] md:w-[50%] sm:w-[80%]">
-      <img src="../../public/images/experience-image.png" alt="my experience" />
-    </div>
-  );
-};
-
-export default ExperienceTopMiddle;
