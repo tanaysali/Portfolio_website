1359937a6791b850b3d97668bbaf835b8a52bcdc
tanaysali <tanaysali2002@gmail.com>
2025-05-29 17:46:32 +0530

    Initial 1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio_website/', // 👈 Replace with your actual repo name
+  base: '/Portfolio_website/', 
   plugins: [react()],
 })
